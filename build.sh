45c88779a5d76cdbc314660e5b89ee595c6ef24e
Dustin Jenkins <djenkins.cadc@gmail.com>
2018-11-22 16:34:09 -0800

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 ENV=${1:-prod}
+DOCKER_IMAGE="jekyll/jekyll:builder"
 echo "Building for ${ENV}"
 sed -i -e 's/^site_env:.*$/site_env: '"${ENV}"'/' _config.yml
-docker run --rm -t -v $(pwd):/srv/jekyll jekyll/jekyll:builder bash -c "bundle install && bundle exec jekyll build"
+
+docker pull ${DOCKER_IMAGE}
+docker run --rm -t -v $(pwd):/srv/jekyll ${DOCKER_IMAGE} bash -c "bundle install && bundle exec jekyll build"
