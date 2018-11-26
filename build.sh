910d9b43aa35dccebd23d581c44fe48835373b82
Dustin Jenkins <djenkins.cadc@gmail.com>
2018-11-26 15:56:14 -0800

    Typo.

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ sed -i -e 's/^site_env:.*$/site_env: '"${ENV}"'/' _config.yml
 docker pull ${DOCKER_IMAGE}
 PWD=$(pwd)
 echo "Mounting ${PWD} to /srv/jekyll"
-docker run --rm -t -v $(pwd):/srv/jekyll -e JEKYLL_UID=97 -e JEKYLL_GID=90 -v /builds:/builds ${DOCKER_IMAGE} bash -c "bundle install && bundle exec jekyll build -d /builds"
+docker run --rm -t -v $(pwd):/srv/jekyll -e JEKYLL_UID=90 -e JEKYLL_GID=97 -v /builds:/builds ${DOCKER_IMAGE} bash -c "bundle install && bundle exec jekyll build -d /builds"
