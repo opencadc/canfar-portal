01cf7f4059e0de10f27ad3c543afd54f328d911c
Dustin Jenkins <djenkins.cadc@gmail.com>
2018-11-26 14:41:05 -0800

    Debugging and fixes.

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ echo "Building for ${ENV}"
 sed -i -e 's/^site_env:.*$/site_env: '"${ENV}"'/' _config.yml
 
 docker pull ${DOCKER_IMAGE}
-docker run --rm -t -v $(pwd):/srv/jekyll:ro -v /srv/cadc/build:/srv/cadc/build ${DOCKER_IMAGE} bash -c "bundle install && bundle exec jekyll build -d /srv/cadc/build"
+PWD=$(pwd)
+echo "Mounting ${PWD} to /srv/jekyll"
+docker run --rm -t -v $(pwd):/srv/jekyll ${DOCKER_IMAGE} bash -c "bundle install && bundle exec jekyll build"
