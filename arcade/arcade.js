d5723548622e2d8f869000cdce5f9c6dc13e06b4
Helena Jeeves <Helena.Jeeves@nrc-cnrc.gc.ca>
2018-12-03 15:33:41 -0800

    s2398, ta 10647: rework of arcade so it automatically forwards to any existing session, or creates a session and forwards to it.

--- arcade/arcade.js
+++ arcade/arcade.js
@@ -1,174 +1,170 @@
+//
+// arcade.js
+//
+// Controller for Arcade VNC Session launching app.
+//
+
 $(document).ready(function() {
-  var sessionTable = $('#session_table').dataTable({
-    ajax: {
-      type: 'GET',
-      url: 'http://arcade.canfar.net/quarry/session',
-      xhrFields: { withCredentials: true },
-      dataType: 'text',
-      error: function(message) {
-        if (
-          $('.databench-authenticated').hasClass('d-none') ||
-          message.status === 0
-        ) {
-          // not logged in, shouldn't have performed this call so quietly continue
-        } else {
-          handleAjaxFail(0, message)
-        }
-      },
-      dataSrc: function(data) {
-        var jsonTableData = []
-        var dataArray = data.split('\n')
-        for (i = 0; i < dataArray.length - 1; i++) {
-          var rowData = dataArray[i].split('\t')
-          var tmpJson = {
-            Name:
-              '<a href="' +
-              rowData[2] +
-              '" target="_blank">' +
-              rowData[1] +
-              '</a>',
-            ID: rowData[0],
-            Uptime: rowData[3],
-            Action:
-              '<i class="fas fa-ban" sessionid="' +
-              rowData[0] +
-              '" sessionname="' +
-              rowData[1] +
-              '"></i>'
-          }
-          jsonTableData.push(tmpJson)
-        }
-        setProgressBar(false)
-        return jsonTableData
-      }
-    },
-    columns: [
-      { data: 'Name' },
-      { data: 'ID' },
-      { data: 'Uptime' },
-      { data: 'Action' }
-    ],
-    columnDefs: [{ width: 20, targets: 3 }]
-  })
-
-  // Add listeners
-
-  // Required so that delete icon function works after ajax data refresh
-  // https://datatables.net/reference/event/init
-  $('#session_table').on('init.dt', function() {
-    setProgressBar(false)
-    addDeleteListeners()
-  })
-
-  // From cadc.user.js. Listens for when user logs in
-  // userManager.subscribe(cadc.web.events.onUserLoad, function(event, data) {
-  //$(document).on('onUserLoad', function(event, data) {
-  //  // Check to see if user is logged in or not
-  //  if (typeof data.error != 'undefined') {
-  //    var errorMsg = ''
-  //    if (data.errorStatus === 401) {
-  //      errorMsg =
-  //        '<em>' +
-  //        data.errorStatus +
-  //        ' ' +
-  //        data.error +
-  //        '</em>. Please log in to use Databench.'
-  //    } else {
-  //      errorMsg =
-  //        'Unable to list sessions: ' + data.errorStatus + ' ' + data.error
-  //    }
-  //    setInfoPanel(errorMsg)
-  //  } else {
-  //    setSessionPanel()
-  //  }
-  //})
-
-  $(document).ready(function () {
-    //checkAuthentication()
-
-  })
-
-  function checkAuthentication() {
-    userManager = new cadc.web.UserManager()
-
-    // From cadc.user.js. Listens for when user logs in
-    userManager.subscribe(cadc.web.events.onUserLoad,
-        function (event, data) {
-          // Check to see if user is logged in or not
-          if (typeof(data.error) != 'undefined') {
-            setNotAuthenticated()
-          } else {
-            setAuthenticated()
-          }
-        })
-
-  }
-
-
-
-
-  $('.table-refresh').click(function() {
-    $('#refreshButton').click(function() {
-      $(this).addClass('fa-spin')
-      var el = $(this)
-      fleetTable.ajax.reload(function() {
-        el.removeClass('fa-spin')
-      })
-    })
-
-    reloadSessionTable()
-  })
-
-  $('.session-add').submit(function() {
-    var $_form = $(this)
-    var formData = $_form.serialize()
-    addDatabenchSession(formData)
-
-    // allow form submit to be ajax, performed in addDatabenchSession
-    return false
-  })
-
-  // From delete modal
-  $('#delete_ok').click(function() {
-    rmDatabenchSession($('.delete-session-id').text())
-
-    // Leave panel up until ajax call returns
-    false
-  })
-}) // end $(document).ready...
-
-
+  getDatabenchSession()
+})
 
 // ---------------- Page load functions ---------------
+
 // #auth_modal is in _page_header.html
 // the other items are expected to be in index_en.md
 function setNotAuthenticated() {
   $('#authen_modal').modal('show')
-  $('.arcade-authenticated').addClass('hidden')
-  $('.arcade-not-authenticated').removeClass('hidden')
+  $('.arcade-authenticated').addClass('d-none')
+  $('.arcade-not-authenticated').removeClass('d-none')
 
   $('#arcade_login_button').click(function() {
     $('#authen_modal').modal('show')}
   )
 }
 
-function setAuthenticated() {
-  // adding a comment... to see how this code is cached during build...
-  $('.arcade-authenticated').removeClass('hidden')
-  $('.arcade-not-authenticated').addClass('hidden')
-  trigger(_selfCitationPage, cadc.web.citation.events.onAuthenticated, {})
-}
-
 // ---------------- arcade.canfar.net ajax functions & response handlers ---------------
 
-function reloadSessionTable() {
-  // Pass in addDeleteListeners() as the callback so the
-  // delete icons will work
-  setProgressBar(true)
-  $('#session_table')
-    .DataTable()
-    .ajax.reload(addDeleteListeners)
+function handleAjaxFail(message) {
+  setProgressBar(false)
+  $('#infoModal').modal('hide')
+  $('.session-failed').removeClass('d-none')
+  $('.session-started').addClass('d-none')
+  $('.session-starting').addClass('d-none')
+
+  var errorMsg = ''
+  switch (message.status) {
+    case 401:
+    case 403:
+      setNotAuthenticated()
+      break
+    default:
+      errorMsg =
+        'Unable to list sessions: ' +
+        message.status +
+        ':  ' +
+        message.responseText
+      setInfoModal('Error', errorMsg, false, true)
+      break
+  }
 }
+
+function forwardTo(sessionName) {
+  // Forward to the session
+  if (typeof sessionName !== "undefined" && sessionName !== '') {
+    setInfoModal(
+        'Forwarded to session ',
+        'Please refresh page to access...',
+        false,
+        true
+    )
+    // Close modal, leave message about refreshing this page to access the session.
+    $('#infoModal').modal('hide')
+    $('.session-starting').addClass('d-none')
+    $('.session-started').removeClass('d-none')
+    setProgressBar('okay')
+    window.open(sessionName, '_blank')
+  }
+}
+
+function postSession(formData) {
+  setInfoModal(
+      'Please wait ',
+      'Establishing session.. (may take up to 10 seconds)',
+      true,
+      false
+  )
+
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest()
+
+    // 'load' is the XMLHttpRequest 'finished' event
+    request.addEventListener(
+        'load',
+        function () {
+          if (request.status == "200") {
+              // forward to the session
+            setProgressBar(true)
+            setInfoModal(
+                'Please wait ',
+                'Forwarding to session...',
+                false,
+                false
+            )
+            resolve(request.responseURL)
+          } else {
+            reject(request)
+          }
+        },
+        false
+    )
+    request.withCredentials = true
+    request.open('POST', 'http://arcade.canfar.net/quarry/session')
+    request.send(formData)
+  })
+}
+
+function getDatabenchSession() {
+  getSession().then(sessionURL => forwardTo(sessionURL))
+      .catch(message => handleAjaxFail(message))
+}
+
+function getSession() {
+  setProgressBar(true)
+  setInfoModal(
+      'Please wait ',
+      'Checking for existing session...',
+      true,
+      false
+  )
+
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest()
+
+    // 'load' is the XMLHttpRequest 'finished' event
+    request.addEventListener(
+        'load',
+        function () {
+          if (request.status == "200") {
+            if (request.responseText === '') {
+              // request a session
+              // have to put another embedded promise in here, I think... not sure
+              // how that will play out with the outer ones, though. :(
+
+              var params = new FormData()
+              params.append( 'name', 'hjeeves-test')
+              postSession(params).then(sessionName => forwardTo(sessionName))
+                  .catch(message => handleAjaxFail(message))
+            } else {
+              // Get URL to forward to
+              var dataArray = request.responseText.split('\n')
+
+              var sessionURL = ""
+              for (i = 0; i < dataArray.length - 1; i++) {
+                var rowData = dataArray[i].split('\t')
+                sessionURL = rowData[2]
+              }
+
+              setInfoModal(
+                  'Please wait ',
+                  'Session found, forwarding...',
+                  false,
+                  false
+              )
+            }
+            resolve(sessionURL)
+          } else {
+            reject(request)
+          }
+        },
+        false
+        )
+    request.withCredentials = true
+    request.open('GET', 'http://arcade.canfar.net/quarry/session')
+    request.send(null)
+  })
+}
+
+// ------------ Panel & modal management functions --------------
 
 function setProgressBar(busy) {
   if (busy === true) {
@@ -178,115 +174,7 @@ function setProgressBar(busy) {
   }
 }
 
-function handleAjaxFail(callType, message) {
-  setProgressBar(false)
-
-  //Option: to make sure error messages are correct,
-  // can add specific messages here
-  switch (callType) {
-    case 1:
-    case 2:
-      //  $('#deleteModal').modal('hide');
-      break
-    case 0:
-    default:
-      break
-  }
-
-  var errorMsg = ''
-  switch (message.status) {
-    case 401:
-    case 403:
-      //errorMsg =
-      //  '<em>' +
-      //  message.status +
-      //  ' ' +
-      //  message.responseText +
-      //  '</em>. Please log in to use Databench.'
-      //setInfoPanel(errorMsg)
-      setNotAuthenticated()
-      break
-    default:
-      errorMsg =
-        'Unable to list sessions: ' +
-        message.status +
-        ':  ' +
-        message.responseText
-      setInfoModal('Error', errorMsg, false)
-      break
-  }
-}
-
-function addDatabenchSession(formData) {
-  setProgressBar(true)
-  setInfoModal(
-    'Pease wait ',
-    'Processing request... (may take up to 10 seconds)',
-    true
-  )
-  $.ajax({
-    xhrFields: { withCredentials: true },
-    url: 'http://arcade.canfar.net/quarry/session',
-    method: 'POST',
-    data: formData
-  })
-    .done(function(data) {
-      $('#infoModal').modal('hide')
-      // clear form
-      $('.session-add input').val('')
-      // Refresh session list
-      reloadSessionTable()
-    })
-    .fail(function(message) {
-      // Option: possibly put error beside form
-      // setFormError(message.status, "Unable to add session: " + message.status + ": " + message.responseText);
-      handleAjaxFail(1, message)
-    })
-}
-
-function rmDatabenchSession(sessionID) {
-  setProgressBar(true)
-  clearDeleteModal()
-  setInfoModal('Pease wait ', 'Processing request...', true)
-  $.ajax({
-    xhrFields: { withCredentials: true },
-    url: 'http://arcade.canfar.net/quarry/session/' + sessionID,
-    method: 'DELETE'
-  })
-    .done(function(data) {
-      // Refresh session list
-      $('#infoModal').modal('hide')
-      reloadSessionTable()
-    })
-    .fail(function(message) {
-      handleAjaxFail(2, message)
-    })
-}
-
-function addDeleteListeners() {
-  $('.fa-ban').click(function() {
-    setDeleteModal(
-      this.getAttribute('sessionid'),
-      this.getAttribute('sessionname')
-    )
-  })
-}
-
-// ------------ Panel & modal management functions --------------
-
-function setInfoPanel(errorMsg) {
-  $('.info-span').html(errorMsg)
-  $('.databench-authenticated').addClass('d-none')
-  $('.databench-anonymous').removeClass('d-none')
-}
-
-function setSessionPanel() {
-  $('.databench-authenticated').removeClass('d-none')
-  $('.databench-anonymous').addClass('d-none')
-  reloadSessionTable()
-}
-
-function setInfoModal(title, msg, hideThanks) {
+function setInfoModal(title, msg, hideThanks, hideSpinner) {
   $('.info-span').html(msg)
   $('#infoModalLongTitle').html(title)
   $('#infoModal').modal('show')
@@ -296,20 +184,10 @@ function setInfoModal(title, msg, hideThanks) {
   } else {
     $('#infoThanks').removeClass('d-none')
   }
-}
 
-function setDeleteModal(sessionId, sessionName) {
-  $('#deleteModal').modal('show')
-  $('.delete-name').html(sessionName)
-  $('.delete-session-id').html(sessionId)
-}
-
-function clearDeleteModal() {
-  $('#deleteModal').modal('hide')
-  $('.delete-name').html('')
-  $('.delete-session-id').html('')
-}
-
-function setFormError(title, msg) {
-  $('.error-span').html(msg)
+  if (hideSpinner === true) {
+    $('.fa-spin').addClass('d-none')
+  } else {
+    $('.fa-spin').removeClass('d-none')
+  }
 }
--- arcade/index_en.md
+++ arcade/index_en.md
@@ -6,64 +6,37 @@ permalink: /en/arcade/
 
 <!-- Content starts -->
 
-<div class="databench-authenticated">
 <section>
-  <h2 class="databench-title">Sessions</h2>
+  <h2 class="arcade-title">Arcade</h2>
 </section>
+<div class="arcade-authenticated">
 <section id="main_section">
     <div class="session-list card panel-default">
-      <nav class="navbar navbar-expand-sm" id="navbar-functions">
-        <ul class="nav navbar-nav arcade-authenticated">
-            <li class="nav-item dataTables_filter">
-              <form class="session-add form-inline">
-                <div class="form-group mx-sm-3 mb-2">
-                  Create new session:
-                  <label for="sessionName" class="sr-only">Session Name</label>
-                  <input class="form-control session-add-control" id="sessionName" name="name" placeholder="Session Name">
-                </div>
-                <button type="submit" class="fa fa-plus btn btn-primary mb-2"></button>
-                <div class="form-group" id="errorDiv"><span class="error-span"></span></div>
-              </form>
-            </li>
-            <li class="nav-item">
-              <button type="submit" class="fa fa-sync table-refresh btn btn-light"></button>
-            </li>
-        </ul>
-      </nav>
+      <div class="panel-heading doi-panel-heading">
+        <h4 class="arcade-modal-title session-starting">Starting session</h4>
+        <h4 class="arcade-modal-title session-started d-none">Session started</h4>
+        <h4 class="arcade-modal-title session-failed d-none">Could not start session</h4>
+      </div>
       <div class="progress session-table-progress progress-bar-striped">
         <div class="cadc-progress progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="100" aria-valuemax="100">
         </div>
       </div>
-      <div class="not-authenticated hidden"><button type="submit" class="btn btn-primary" id="arcade_login_button">
-        <i>Login Required...</i></button>
-      </div>
-      <div id="sessions" class="table-responsive arcade-authenticated">
-        <div class="card-body">
-           <table id="session_table" class="table table-sm table-hover table-responsive-md dataTable">
-             <thead>
-               <tr>
-                 <th>Name</th>
-                 <th>ID</th>
-                 <th>Uptime</th>
-                 <th></th>
-               </tr>
-             </thead>
-             <tbody>
-             </tbody>
-           </table>
+      <div class="panel-body doi-panel-body">
+        <div class="arcade-not-authenticated d-none">
+          <button type="submit" class="btn btn-primary" id="arcade_login_button">
+            <i>Login Required</i>
+          </button>
         </div>
-    </div>
-    </div></section>
+        <div class="forwarded session-started d-none">
+          <i>Please refresh page to access current session.</i>
+        </div>
+        <div class="forwarded session-failed d-none">
+          <i>Try reloading page to refresh session, or contact a CANFAR adminitrator.</i>
+        </div>
+      </div>
+    </div>     
+</section>
 </div>
-
-<!-- TODO: remove this when login #auth-modal from header is used
-<!-- <div class="databench-anonymous"> -->
-<!--     <div class="info-panel card panel-default"> -->
-<!--         <div class="card-body"> -->
-<!--             <span class="info-span"></span> -->
-<!--         </div> -->
-<!--     </div> -->
-<!-- </div> -->
 
 <!-- Info/Error Modal -->
 <!-- Displayed when anything other than a 401 or 200 is returned -->
@@ -78,76 +51,10 @@ permalink: /en/arcade/
       </div>
       <div class="modal-body">
         <span class="info-span"></span>
+        <span class="arcade-spinner fas fa-spinner fa-spin"></span>
       </div>
       <div id="infoThanks" class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Thanks</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<!-- Confirm Modal -->
-<!-- Displayed on delete for confirmation -->
-<div class="modal" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModal" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="deleteModalLongTitle">Confirm Delete</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        Are you sure you want to delete session <span class="delete-name"></span> (<span class="delete-session-id"></span>)?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="button" id="delete_ok" class="btn btn-secondary" data-dismiss="modal">OK</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-
-<!-- Login Modal -->
-<!-- Has to be here, not in the header, so the modal background works correctly...   -->
-<!-- Question will come up when this has to be generalized and used by other apps... -->
-<div class="modal fade" id="authen_modal" tabindex="-1" role="dialog" aria-labelledby="auth_modal_label">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header canfar-left-align">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <h5 class="modal-title" id="auth_modal_label">Authentication required
-        </h5>
-      </div>
-      <div class="modal-body">
-        <form class="access-control" id="modalloginForm" role="form" method="post" action="/access/login">
-          <div class="modal-body">
-            <span id="modal_login_fail" class="text-danger help-block pull-left"></span>
-            <div class="form-group">
-              <label for="username" class="d-none" id="modalUsernameLabel">Username</label>
-              <input type="text" id="modalUsername" name="username" class="form-control" tabindex="1" required="required" placeholder="Username"
-              />
-            </div>
-            <div class="form-group">
-              <label for="password" class="d-none" id="modalPasswordLabel">Password</label>
-              <input type="password" id="modalPassword" name="password" class="form-control" tabindex="2" required="required" placeholder="Password"
-              />
-            </div>
-            <a href="/canfar/auth/forgot.html" tabindex="5" class="account_access_info" title="Forgot Username" id="forgot_username_2">
-              Forgot your Account information?</a>
-            <br/>
-            <a href="/canfar/auth/request.html" tabindex="6" class="account_access_info" title="Register" id="register_cadc_2">
-              Request an Account</a>
-          </div>
-          <div class="modal-footer">
-            <button type="submit" class="btn btn-success">
-              <span class="fa fa-sign-in-alt"></span> Login
-            </button>
-          </div>
-        </form>
       </div>
     </div>
   </div>
@@ -159,11 +66,13 @@ permalink: /en/arcade/
 
 <!-- fontawesome *should* be included in the header -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
-
-<!-- DataTables includes this page requires -->
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" integrity="sha384-EkHEUZ6lErauT712zSr0DZ2uuCmi3DoQj6ecNdHQXpMpFNGAQ48WjfXCE5n20W+R" crossorigin="anonymous">
 <link rel="stylesheet" href="/css/canfar.css">
 
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" integrity="sha384-rgWRqC0OFPisxlUvl332tiM/qmaNxnlY46eksSZD84t+s2vZlqGeHrncwIRX7CGp" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js" integrity="sha384-uiSTMvD1kcI19sAHJDVf68medP9HA2E2PzGis9Efmfsdb8p9+mvbQNgFhzii1MEX" crossorigin="anonymous"></script>
+<script type="text/javascript" src="http://apps.canfar.net/cadcJS/javascript/org.opencadc.js"></script>
+<script type="text/javascript" src="http://apps.canfar.net/cadcJS/javascript/cadc.uri.js"></script>
+<script type="text/javascript" src="http://apps.canfar.net/canfar/javascript/cadc.user.js"></script>
+
+<script type="text/javascript" src="//www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/cadc/bootstrap/js/cadc.user.js"></script>
+<script type="text/javascript" src="//www.cadc-ccda.hia-iha.nrc-cnrc.gc.ca/cadc/bootstrap/js/login.js"></script>
+
 <script type="text/javascript" src="/arcade/arcade.js"></script>
