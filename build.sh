e491db0bb964abfd4d813316787f2747a1c48199
Dustin Jenkins <djenkins.cadc@gmail.com>
2025-09-08 15:43:56 -0700

    fix: fix typos and add flag for the red banner

--- build.sh
+++ build.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 ENV=${1:-prod}
-DOCKER_IMAGE=${DOCKER_IMAGE:-"jekyll/builder:4"}
+DOCKER_IMAGE=${DOCKER_IMAGE:-"ruby:3.4"}
 echo "Building for ${ENV}"
 sed -i -e 's/^site_env:.*$/site_env: '"${ENV}"'/' _config.yml
 
-docker pull ${DOCKER_IMAGE}
 PWD=$(pwd)
 OUTPUT_DIR="${PWD}/_site"
 echo "Building into ${OUTPUT_DIR}"
-docker run --rm -t -v $(pwd):/srv/jekyll -v ${HOME}/.bundle:/home/jekyll/.bundle ${DOCKER_IMAGE} /bin/sh -c "bundle install && bundle exec jekyll build"
+docker run --rm -t -v $(pwd):/srv/jekyll -w /srv/jekyll ${DOCKER_IMAGE} /bin/sh -c "bundle install && bundle exec jekyll build"
 
 if [[ -d "${OUTPUT_DIR}" && ! -z $RPS ]];
 then
   rsync -avc ${OUTPUT_DIR}/* ${RPS}/www/
 else
-  echo "Nothing built in ${OUTPUT_DIR}"
+  echo "Not sending build to $RPS (does not exist)"
 fi
--- run.sh
+++ run.sh
@@ -10,9 +10,9 @@ echo "Starting Jekyll with Docker in daemon mode as uid ${RUN_UID}"
 docker run --name jekyll --rm -t -p 4000:4000 \
   -e JEKYLL_UID="${RUN_UID}" \
   -v $(pwd):/srv/jekyll \
-  -v ${HOME}/.bundle:/home/jekyll/.bundle \
-  jekyll/jekyll:4 \
-  /bin/sh -c "bundle install && jekyll serve --host 0.0.0.0"
+  -w /srv/jekyll \
+  ruby:3.4 \
+  /bin/sh -c "bundle install && bundle exec jekyll serve --host 0.0.0.0"
 
 # Wait for Jekyll Docker container to stop
 wait $!
